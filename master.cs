eb9b9860ff07d2d55e8ccf7fc581fb2864e6b189
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-08 12:50:49 +0530

    inital commit

---
+++ master.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    internal class master
+    {
+        public static void main(string[] args)
+        {
+
+        }
+    }
+}
